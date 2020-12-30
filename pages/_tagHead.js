d72c0c6e1852e5111c84bb6a5eb5ff607383129f
yokuny <felipe.vni@hotmail.com>
2020-12-30 17:21:43 -0300

    animations

--- pages/_tagHead.js
+++ pages/_tagHead.js
@@ -1,8 +1,9 @@
-import Head from 'next/head'
+import Head from 'next/head';
 export default function TagHead() {
   return (
     <Head>
-      <title>Yokuny |</title>
+      <title>Yokuny | project pages</title>
+      <link rel="shortcut icon" href="/page-icon.svg" type="image/svg" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import GridArea from './GridArea'
+import TagHead from './_tagHead'
 export default function Home() {
   return (
-    <GridArea />
+    <>
+      <TagHead />
+      <GridArea />
+    </> 
   );
 };
