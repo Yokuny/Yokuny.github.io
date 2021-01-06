a58f8f93654a7a7c99d4af42ef5fd6dd1e19945a
yokuny <felipe.vni@hotmail.com>
2021-01-06 18:51:29 -0300

    now the site have a logo

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import GridAreaPlan from './GridAreaPlan'
 import TagHead from './_tagHead'
+import Image from 'next/image';
 export default function Home() {
   return (
     <>
+      <div className="mb-8 flex justify-center items-cente lg:max-h-20">
+        <Image
+          src="/saint-machine-animated-VHS.svg"
+          alt='mail image'
+          width={800}
+          height={100}
+        />
+      </div>
       <TagHead />
       <GridAreaPlan />
     </> 
