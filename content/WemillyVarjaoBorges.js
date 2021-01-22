eafddf809e704bb74d661a90b7c462a7aef4e39b
yokuny <felipe.vni@hotmail.com>
2021-01-22 09:09:34 -0300

    Wemilly Varjão Borges new page

---
+++ content/WemillyVarjaoBorges.js
@@ -0,0 +1,21 @@
+const WemillyVarjaoBorges = {
+    file:`WemillyVarjaoBorges`,
+    title:`Wemilly Varjão Borges`,
+    projectLink:`https://www.behance.net/gallery/111774865/Wemilly-Varjao-Borges`,
+    platform:`Behance`,
+    socialNetworkNamesAndLink:[
+        [`instagram`,`https://www.instagram.com/wemilly_v.b/`],[`facebook`,`asdasd`]
+    ],
+    imgSrc:[
+        `https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/a45b68111774865.600836c472d19.jpg`,
+        `https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/c27e82111774865.600836c473bd9.jpg`,
+        `https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/d54cec111774865.600836c47474a.jpg`,
+        `https://mir-s3-cdn-cf.behance.net/project_modules/fs/2e3456111774865.600836c4733c9.jpg`,
+        `https://mir-cdn.behance.net/v1/rendition/project_modules/2800_opt_1/ff9953111774865.600836c475269.jpg`,
+        `https://mir-cdn.behance.net/v1/rendition/project_modules/2800_opt_1/8771cc111774865.600836c475c10.jpg`
+    ],
+    comment:[
+        `Primeiro comentario`,`continuação do comentario`,`referencias se forem necessarias`
+    ]
+};
+export default WemillyVarjaoBorges;
--- utils/allPostsDataGet.js
+++ utils/allPostsDataGet.js
@@ -1,10 +1,13 @@
 import FelipeRangelRibeiro from '../content/FelipeRangelRibeiro'
 import TiagoRangelRibeiro from '../content/TiagoRangelRibeiro'
 import MaryangelaCesconettoRainha from '../content/MaryangelaCesconettoRainha'
+import WemillyVarjaoBorges from '../content/WemillyVarjaoBorges'
 export default function allPostsDataGet(){
-    let allData = [];
+    //mudei para const. olhar se nao vai dar erro
+    const allData = [];
     allData.push(TiagoRangelRibeiro);
     allData.push(FelipeRangelRibeiro);
     allData.push(MaryangelaCesconettoRainha);
+    allData.push(WemillyVarjaoBorges);
     return allData;
 }
