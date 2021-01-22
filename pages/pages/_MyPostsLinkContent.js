9bd050df54ee2503c2154fd997d44cbd732d721f
yokuny <felipe.vni@hotmail.com>
2021-01-21 22:14:45 -0300

    im right the links are not accessible, because we cant put a a tag instantly next a Link tag

--- pages/pages/_MyPostsLinkContent.js
+++ pages/pages/_MyPostsLinkContent.js
@@ -4,16 +4,14 @@ export default function LinkContent(){
     const allPostsData = allPostsDataGet();
     return(
         <>
-            {allPostsData.map(({ file, title, platform }) => (
-                <li key={file}>
+            {allPostsData.map(({ file, title, platform }) => 
                 <Link href={`/${file}`}>
-                <>
-                    <a className="text-green-600 hover:bg-green-600 hover:text-black duration-500">{title}</a>
-                    <a className="mx-4 text-lightBlue-400 hover:bg-lightBlue-400 hover:text-black duration-500">{platform}</a>
-                </>
+                    <li key={file} className="cursor-pointer">
+                        <a className="text-green-600 hover:bg-green-600 hover:text-black duration-500">{title}</a>
+                        <a className="mx-4 text-lightBlue-400 hover:bg-lightBlue-400 hover:text-black duration-500">{platform}</a>
+                    </li>
                 </Link>
-                </li>
-            ))}
+            )}
         </>
     )
 }
