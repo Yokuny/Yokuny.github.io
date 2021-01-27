e1f4b736b8d6a3fe15ae278712543fdd04327661
yokuny <felipe.vni@hotmail.com>
2021-01-26 21:17:58 -0300

    felipe rangel page, has changed

--- content/FelipeRangelRibeiro.js
+++ content/FelipeRangelRibeiro.js
@@ -1,7 +1,7 @@
 const FelipeRangelRibeiro = {
     file:"FelipeRangelRibeiro",
     title:`Felipe Rangel Ribeiro`,
-    projectLink:`https://www.behance.net/gallery/110190745/trail`,
+    projectLink:`https://www.behance.net/gallery/110190745/Felipe-Rangel-Ribeiro`,
     platform:`Behance`,
     socialNetworkNamesAndLink:[
         [`instagram`,`https://www.instagram.com/user09111996/`],
@@ -17,7 +17,11 @@ const FelipeRangelRibeiro = {
         `https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/c861e0110190745.5fe65f794a287.png`
     ],
     comment:[
-        `Primeiro comentario`,`continuação do comentario`,`referencias se forem necessarias`
+        `A explicação da minha recusa e do desamor estava no meu gênio solitário e contemplativo. Até aos quinze anos fui tido por idiota; dos quinze aos vinte chamavam-me poeta; e, se as palavras eram diferentes, o sentido que a minha família lhes dava era o mesmo. Era pouco de ser estimado um moço que não comungava nos mesmos passatempos da casa e via correr as horas na leitura e nas digressões pelo mato.`,
+        `Estou só. Ouço bater o mar que se quebra na praia a cinqüenta passos de mim. É o único rumor que nesta hora quebra o silêncio da noite. Fora desse sinto apenas o leve ruído da pena que corre no papel. Escrevo sem assunto e em busca de assunto. Que há de ser? Sobre a mesa tenho duas pilhas de livros. De um lado a Bíblia e Pascal, do outro Alfredo de Vigny e Lamartine. É obra do acaso e não parece: tal é o estado do meu espírito. Os três primeiros livros me chamam à contemplação ascética e às reflexões morais; os três últimos despertam os sentimentos do coração e levam meu espírito às mais elevadas regiões da fantasia.`,
+        `Quero entranhar-me no mundo da reflexão e do estudo, mas o meu coração, solteiro talvez, talvez viúvo, pede-me versos ou imaginações. Triste alternativa, que para nenhuma resolução me guia! Este estado, tão comum nos que realmente se dividem entre sentir e pensar, é uma dor d’alma, é uma agonia do espírito.`,
+        `-`,
+        `Felicidade pelo Casamento - Machado de Assis`
     ]
 };
 export default FelipeRangelRibeiro;
