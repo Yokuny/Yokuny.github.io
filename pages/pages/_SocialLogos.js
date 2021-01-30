bbaa7135e624cd0b8287a601c60e555e6863eec2
yokuny <felipe.vni@hotmail.com>
2021-01-29 23:06:38 -0300

    have changed the link to my instagram and the page logo

--- pages/pages/_SocialLogos.js
+++ pages/pages/_SocialLogos.js
@@ -3,7 +3,7 @@ export default function SocialLogos(){
     const socialLogos = [
         {src:'/github_animated.svg', alt:'github logo', link:'https://github.com/Yokuny', rotate:'transform hover:translate-y-6 hover:-rotate-45 duration-500', key:"1"},
         {src:'/whatsapp_animated.svg', alt:'whatsapp logo', rotate:'transform hover:translate-y-6 hover:rotate-45 duration-500', key:"2"},
-        {src:'/instagram_animated.svg', alt:'instagram logo', link:'https://www.instagram.com/user09111996/', rotate:'transform hover:translate-y-6 hover:rotate-90 duration-500', key:"3"},
+        {src:'/instagram_animated.svg', alt:'instagram logo', link:'https://www.instagram.com/_saint_machine_/', rotate:'transform hover:translate-y-6 hover:rotate-90 duration-500', key:"3"},
         {src:'/behance_animated.svg', alt:'behance logo', link:'https://www.behance.net/Yokuny', rotate:'transform hover:translate-y-6 hover:-rotate-180 duration-500', key:"4"},
         {src:'/linkedin_animated.svg', alt:'linkedin logo', link:'https://www.linkedin.com/in/yokuny/', rotate:'transform hover:translate-y-6 hover:-rotate-12 duration-500', key:"5"},
       ];
