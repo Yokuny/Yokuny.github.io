d1989366c2c99bf9cc6ac4ed4004333f1bce0d09
yokuny <felipe.vni@hotmail.com>
2021-01-30 23:15:25 -0300

    just a adjust in the augusto post

--- content/AugustoCesarDeAndradeFarias.js
+++ content/AugustoCesarDeAndradeFarias.js
@@ -20,7 +20,7 @@ const AugustoCesarDeAndradeFarias = {
     ],
     comment:[
         `Com fogos corrosivos, ele escreveu a seguinte sentença, agora percebida pelas mentes dos homens e lida por eles na Terra:`,
-        `Como você poderia saber que cada Pássaro que corta a estrada dos ares E um imensomundo de deleite, bloqueado por seus cinco sentidos?`,
+        `Como você poderia saber que cada Pássaro que corta a estrada dos ares e um imenso mundo de deleite, esta bloqueado por seus cinco sentidos?`,
         `Na semeadura aprenda, na colheita ensine, no inverno desfrute. Guie sua carroça e seu arado sobre os ossos dos mortos. A estrada do excesso leva ao palácio da sabedoria. A prudência é uma velha donzela feia cortejada pela Incapacidade. Quem deseja, mas não age, gera a pestilência. A minhoca cortada perdoa o arado. Mergulha no rio quem ama a água. Um tolo não vê a mesma árvore vista por um sábio. Aquele cuja face não tem luz nunca se tornará uma estrela. A eternidade está apaixonada pelas produções do tempo. A abelha ocupada não tem tempo para a tristeza. As horas de tolice são medidas pelo relógio,
         mas as de sabedoria, nenhum relógio pode medir.`,
         `Toda comida saudável é apanhada em rede ou arapuca. Ressalte os números de peso e medida em um ano de escassez. Nenhum pássaro voa alto demais, se voa com as próprias asas. Um corpo morto não se vinga de ofensas. O mais sublime ato é pôr outro antes de você. Se o tolo persistisse em sua tolice, ele se tornaria sábio. A tolice é o disfarce da patifaria. A Vergonha é o disfarce do Orgulho.`,
